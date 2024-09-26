279134f552c112bbe33f62e9bc535873cee33e69
gospel <2381043@unai.edu>
2024-09-26 10:05:53 +0700

    Handson 5

---
+++ main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class main {
+    public static String[] todos = new String[10];
+    public static Scanner = new Scanner(System.in);
+
+    Scanner(System.10);
+
+    public static void main(String[] args) {
+        System.out.println("Befor delet");
+        addTodoList
+    }
+
+    public static void showTodoList
+
+    {
+        System.out.println("TODO LIST");
+        for (int i = 0; i < todos.length; i++) {
+            String todo = todos[i];
+            if (todos[i] != null) {
+                System.out.println((i + 1) + ". " + todo);
+            }
+        }
+    }
+if(isFull)
+
+    {
+        String[] temp = todos;
+        todos = new String[todos.length * 2];
+        for (int 1 = 0; i < temp.length ;
+        i++)
+        todos[i] = temp[i]
+    }
+}
+
+for(int i = number - 1; i < todos.length; i++){
+    if (i== (todos.length)) {
+        todos[i] = tosod[i + 1];
+        }
+}
+
+return true;
